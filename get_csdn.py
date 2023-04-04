59d480b7cf4207e3e8701c87746e7144bebed1b2
MPU王荣胜 <fujingshusheng@gmail.com>
2023-04-04 17:58:49 +0800

    Update get_csdn.py

--- get_csdn.py
+++ get_csdn.py
@@ -5,8 +5,19 @@ import bs4
 def get_data():
     # 获取csdn粉丝
     url = 'https://blog.csdn.net/u014297502'
-    headers = {
+    headers1 = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36(KHTHL, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+    }
+    
+    headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Connection': 'keep-alive',
+    'Content-Type': 'text/plain;charset=UTF-8',
+    'Referer': referer,
+    # 'origin': 'https://blog.csdn.net',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3875.400 QQBrowser/10.8.4492.400'
     }
 
     html_file = requests.get(url, headers=headers)
